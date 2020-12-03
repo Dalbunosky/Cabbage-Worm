0be5e89f96ab4d8b444c76e1f8356a2d70b5e641
Chi Yung Lee <chiyung.lee@gmail.com>
2020-12-03 03:02:28 -0800

    worm mostly done. delay in direction change however. Score doesn't work above 4. Crash into self not functioning.

--- snake.js
+++ snake.js
@@ -1,187 +1,45 @@
 // classes board, snake, tile, food
 // Build game:
     // Grab element from HTML
-    const cvs = document.getElementById("snake");
-    const ctx = cvs.getContext("2d");
-    // Initialize variables
-    let score = 0;
-    let highScore = 0;
-    let speed = 1000; // Interval between draws, determines speed of game
-
-    // Height and width will change to depend on user input in the future.
-    // Change to be window width / (user request + 2), so long tile > minimum
-    const height = 10;
-    const width = 10;
-    const tile = 50;
-
-    // Set snake head, use emptySpaces to keep track of empty spaces
-    let snake = [];
-    const emptySpaces = new Set();
-    for(let i = 0; i < width * height; i++){
-        emptySpaces.add(i*j);
-    }
-
-    snake[0] = {
-        x: Math.floor(Math.random()*width+1) * tile,
-        y: Math.floor(Math.random()*height+1) * tile
-    }
-    emptySpaces.delete(snake[0].x * snake[0].y);
-
-    // Set food
-    const foodImg = new Image();
-    foodImg.src = "img/food.png";
-    let foodLocation = setFood();
-    let food = {
-        x: foodLocation[0] * tile,
-        y: foodLocation[1] * tile
-    }
-
-    // Run game
-    draw();
-    let game 
-    // game = setInterval(draw, speed);  // Game won't be using this until otherwise fully functioning
-    game = document.addEventListener("keydown", direction);    // Until game is otherwise running, this is the only way to advance in game
-
-// Run game
-    // Turn based
-    // Draw snake
-    draw();
-    // Game Over?;
-    if(gameOver()){
-        // End Game, TBD, DONT DRAW
-        game = null;
-        // clearInterval(game);
-        if(score > highScore) highScore = score;
-        score = 0;
-    }
-    // Advance
-
-
-    // Eat food?
-        // DON'T POP
-        // Reassign food
-        // update score
-
-// Food assignment
-    function setFood(){
-        let openSpots = [...emptySpaces];
-        let newFoodLocation = openSpots[Math.floor(Math.random() * openSpots.length)];
-        return [Math.floor(newFoodLocation / width), newFoodLocation % width]
-    }
-
-// Draw function
-    function draw(){
-    // Draw canvas
-        ctx.fillStyle = "#FF0000";
-        ctx.fillRect(0,0,(width + 2) * tile, (height + 2) * tile);
-        ctx.fillStyle = "Lightgreen";
-        ctx.fillRect(tile, tile, width * tile, height * tile);
-    // Draw snake
-        for(let i =0; i < snake.length; i++){
-            ctx.fillStyle = (i === 0) ? "orange" : "yellow";
-            ctx.fillRect(snake[i].x, snake[i].y, tile, tile);
-
-            ctx.strokeStyle = "red";
-            ctx.strokeRect(snake[i].x, snake[i].j, tile, tile)
-        }
-    // Draw food
-        // Snake eat? setFood
-        // Draw food
-        ctx.drawImage(foodImg, food.x, food.y, tile, tile);
-
-    // Move snake
-        // old head position
-        let snakeX = snake[0].x;
-        let snakeY = snake[0].y;
-
-        // move
-        if( d = "LEFT") snakeX -= tile;
-        if( d = "UP") snakeY -= tile;
-        if( d = "RIGHT") snakeX += tile;
-        if( d = "DOWN") snakeY += tile;
-
-    // If the snake eats food
-        if(snakeX === food.x && snakeY === food.y){
-        // Add and display new score
-            score ++;
-            scoreUpdate();
-        // Assign new food
-            let newFood = setFood();
-            food = {
-                x: newFood[0] * tile,
-                y: newFood[1] * tile
-            }
-        } else {
-        // remove tail, add to available Set
-            let poop = snake.pop();
-            emptySpaces.add(poop.x * width + poop.y)
-        }
-    // Append new head
-        snake.unshift(
-            {x: snakeX, y: snakeY}
-        )
-    }
-
-// Change direction
-document.addEventListener("keydown", direction);
-    function direction(event){
-        if(event.keyCode === 37 && d != "RIGHT"){
-            d = "LEFT";
-            draw();
-        }
-        else if(event.keyCode === 38 && d != "DOWN"){
-            d = "UP";
-            draw();
-        }
-        else if(event.keyCode === 39 && d != "LEFT"){
-            d = "RIGHT";
-            draw();
-        }
-        else if(event.keyCode === 40 && d != "UP"){
-            d = "DOWN";
-            draw();
-        }
-    }
-
-    function gameOver(){
-        const head = snake[0];
-    // check for snake head collision with wall
-        if(head.x < 1 || head.x > 11 || head.y < 1 || head.y > 11) return true;
-    // check for snake head collision with self
-        // Snake can't hit self if length < 5
-        if(snake.length < 5) return false;
-    // If head location is not in emptySpaces Set, snake has crashed into self
-        if(!emptySpaces.has(head.x * width + head.y)) return true
-
-    // Game not over. Delete new head location from Set
-        emptySpaces.delete(head.x * width + head.y)
-        return false;
-    }
-
-    function scoreUpdate() {
-        document.getElementById('score').innerText = '' + score;
-        // if(score % 5 === 0) speed *= 0.95;
-    };
-
-
-
-
-// classes board, snake, tile, food
-// Build game:
-    // Grab element from HTML
+        const cvs = document.getElementById("snake");
+        const ctx = cvs.getContext("2d");
+        document.addEventListener("keydown", changeDirection);
     // Initialize variables
         // Height and width will change to depend on user input in the future.
-        // Score, highScore keep track of scores
-        // Speed: how fast
         // Tile: width of snake
-        // Active: Is game going on? Prevent height and width from changing mid-game/
-    // Change to be window width / (user request + 2), so long tile > minimum
+        const height = 10, width = 10, tile = 50;
+        // Score, highScore keep track of scores
+        // Speed: Interval between draws, determines speed of game
+        // Active: Is game going on? Prevent height and width from changing mid-game
+        // Direction
+        let score = 0, highScore = 0, defaultSpeed = 1000000000, active = true, direction;
+        let speed = defaultSpeed;
+    // Change tile to be window width / (user request + 2), so long tile > minimum
 
     // Set snake head, use emptySpaces to keep track of empty spaces
+        let snake = [];
+        const emptySpaces = new Set();
+        for(let i = 0; i < width * height; i++){
+            emptySpaces.add(i * tile);
+        }
+
+        snake[0] = {
+            x: Math.floor(Math.random()*width+1) * tile,
+            y: Math.floor(Math.random()*height+1) * tile
+        }
+        // Can't remove from emptySpaces until check has been done, or game will crash
+        // emptySpaces.delete(snake[0].x * width + snake[0].y);
     // Set food
+        const foodImg = new Image();
+        foodImg.src = "img/food.png";
+        // let foodLocation = 
+        let food = setFood();
 
 // Run game
     // Turn every SPEED
+    draw();
+    let game = setInterval(turn, speed);
+
 // Game over
     // clearInterval, Active = false
 
@@ -190,59 +48,128 @@ document.addEventListener("keydown", direction);
     // change direction
     // if active, draw
 
-// FUNCTIONS:
 // Turn
+    function turn(){
     // Draw
-        // Draw canvas
-        // Draw snake
-        // Draw food
+        draw();
     // Game Over?
-        // Head hit wall?
-        // Length > 4?
-            // Head hit self?
+        if(gameOver()){
+        // End Game, TBD, DONT DRAW
         // Active = false
-        // clearInterval(game);
-        //--------------------------
-        // delete head from emptySet
+            clearInterval(game);
+            active = false;
+        // Reassign high score
+        // Reset score
+            if(score > highScore) highScore = score;
+            document.getElementById('highScore').innerText = '' + highScore;
+            score = 0;
+        }
     // Advance
         // Determine new head
-            // old head + direction
+        let snakeX = snake[0].x;
+        let snakeY = snake[0].y;
+        // old head + direction
         // unshift new head
-    // Eat food?
-        // DON'T POP
-        // Reassign food
-        // update score
-        //--------------------------
-        // Pop tail
-        // Add tail to emptySet
+        // Remove head from emptySpaces
+        if(direction){
+            if( direction === "LEFT") snakeX -= tile;
+            else if( direction === "UP") snakeY -= tile;
+            else if( direction === "RIGHT") snakeX += tile;
+            else if( direction === "DOWN") snakeY += tile;
+            snake.unshift({
+                x: snakeX,
+                y: snakeY
+            });
+            emptySpaces.delete((snakeX / tile) * width + (snakeY / tile))
+        // Eat food?
+            if(snakeX === food.x && snakeY === food.y){
+            // update score
+                scoreUpdate();
+            // Reassign food
+                food = setFood();
+            }
+            else{
+            // Pop tail
+            const poop = snake.pop();
+            // Add tail to emptySpaces
+            emptySpaces.add(poop.x * width + poop.y)
+            }        
+        }
 
-// Food assignment
+
+    }
+
+
+// FUNCTIONS:
 
 // Draw function
+    function draw(){
     // Draw canvas
+        ctx.fillStyle = "#FF0000";
+        ctx.fillRect(0,0,(width + 2) * tile, (height + 2) * tile);
+        ctx.fillStyle = "saddlebrown";
+        ctx.fillRect(tile, tile, width * tile, height * tile);
     // Draw snake
+        for(let i =0; i < snake.length; i++){
+            ctx.fillStyle = (i === 0) ? "orange" : "yellow";
+            ctx.fillRect(snake[i].x, snake[i].y, tile, tile);
+
+            ctx.strokeStyle = "red";
+            ctx.strokeRect(snake[i].x, snake[i].j, snake[i].x + tile, snake[i].j + tile);
+        }
     // Draw food
-        // Snake eat? setFood
-        // Draw food
+        ctx.drawImage(foodImg, food.x, food.y, tile, tile);
+    }
 
-    // Move snake
-        // old head position
-
-        // move
-
-    // If the snake eats food
-        // Add and display new score
-        // Assign new food
-        // remove tail, add to available Set
-    // Append new head
+// Food assignment
+    function setFood(){
+        let openSpots = [...emptySpaces];
+        let newFoodLocation = openSpots[Math.floor(Math.random() * openSpots.length)] / tile;
+        // return [Math.floor(newFoodLocation / width), newFoodLocation % width]
+        return {
+            x: tile * (1 + Math.floor(newFoodLocation / width)),
+            y: tile * (1 + newFoodLocation % width)
+        }
+    }
 
 // Change direction
+    document.addEventListener("keydown", changeDirection);
+    function changeDirection(event){
+        if(event.keyCode === 37 && direction != "RIGHT"){
+            direction = "LEFT";
+        }
+        else if(event.keyCode === 38 && direction != "DOWN"){
+            direction = "UP";
+        }
+        else if(event.keyCode === 39 && direction != "LEFT"){
+            direction = "RIGHT";
+        }
+        else if(event.keyCode === 40 && direction != "UP"){
+            direction = "DOWN";
+        }
+        turn();
+    }
+
+// Update score
+    function scoreUpdate() {
+        score ++;
+        document.getElementById('score').innerText = '' + score;
+        // if(score % 5 === 0) speed *= 0.95;
+    };
 
 // Game over?
+    function gameOver(){
+        const head = snake[0];
     // check for snake head collision with wall
+        if(head.x < tile || head.x > (width * tile + tile) || head.y < tile || head.y > (height * tile + tile)) return true;
     // check for snake head collision with self
         // Snake can't hit self if length < 5
+        if(snake.length < 5) return false;
     // If head location is not in emptySpaces Set, snake has crashed into self
+        if(!emptySpaces.has((head.x / tile) * width + (head.y / tile))) return true
 
     // Game not over. Delete new head location from Set
-    // 
+        emptySpaces.delete((head.x / tile) * width + (head.y / tile))
+        return false;
+    }
+    
---
+++ snake1.js
@@ -0,0 +1,248 @@
+// classes board, snake, tile, food
+// Build game:
+    // Grab element from HTML
+    const cvs = document.getElementById("snake");
+    const ctx = cvs.getContext("2d");
+    // Initialize variables
+    let score = 0;
+    let highScore = 0;
+    let speed = 1000; // Interval between draws, determines speed of game
+
+    // Height and width will change to depend on user input in the future.
+    // Change to be window width / (user request + 2), so long tile > minimum
+    const height = 10;
+    const width = 10;
+    const tile = 50;
+
+    // Set snake head, use emptySpaces to keep track of empty spaces
+    let snake = [];
+    const emptySpaces = new Set();
+    for(let i = 0; i < width * height; i++){
+        emptySpaces.add(i);
+    }
+
+    snake[0] = {
+        x: Math.floor(Math.random()*width+1) * tile,
+        y: Math.floor(Math.random()*height+1) * tile
+    }
+    emptySpaces.delete(snake[0].x * snake[0].y);
+
+    // Set food
+    const foodImg = new Image();
+    foodImg.src = "img/food.png";
+    let foodLocation = setFood();
+    let food = {
+        x: foodLocation[0] * tile,
+        y: foodLocation[1] * tile
+    }
+
+    // Run game
+    draw();
+    let game 
+    // game = setInterval(draw, speed);  // Game won't be using this until otherwise fully functioning
+    game = document.addEventListener("keydown", direction);    // Until game is otherwise running, this is the only way to advance in game
+
+// Run game
+    // Turn based
+    // Draw snake
+    draw();
+    // Game Over?;
+    if(gameOver()){
+        // End Game, TBD, DONT DRAW
+        game = null;
+        // clearInterval(game);
+        if(score > highScore) highScore = score;
+        score = 0;
+    }
+    // Advance
+
+
+    // Eat food?
+        // DON'T POP
+        // Reassign food
+        // update score
+
+// Food assignment
+    function setFood(){
+        let openSpots = [...emptySpaces];
+        let newFoodLocation = openSpots[Math.floor(Math.random() * openSpots.length)];
+        return [Math.floor(newFoodLocation / width), newFoodLocation % width]
+    }
+
+// Draw function
+    function draw(){
+    // Draw canvas
+        ctx.fillStyle = "#FF0000";
+        ctx.fillRect(0,0,(width + 2) * tile, (height + 2) * tile);
+        ctx.fillStyle = "Lightgreen";
+        ctx.fillRect(tile, tile, width * tile, height * tile);
+    // Draw snake
+        for(let i =0; i < snake.length; i++){
+            ctx.fillStyle = (i === 0) ? "orange" : "yellow";
+            ctx.fillRect(snake[i].x, snake[i].y, tile, tile);
+
+            ctx.strokeStyle = "red";
+            ctx.strokeRect(snake[i].x, snake[i].j, snake[i].x + tile, snake[i].j + tile);
+        }
+    // Draw food
+        // Snake eat? setFood
+        // Draw food
+        ctx.drawImage(foodImg, food.x, food.y, tile, tile);
+
+    // Move snake
+        // old head position
+        let snakeX = snake[0].x;
+        let snakeY = snake[0].y;
+
+        // move
+        if( d = "LEFT") snakeX -= tile;
+        if( d = "UP") snakeY -= tile;
+        if( d = "RIGHT") snakeX += tile;
+        if( d = "DOWN") snakeY += tile;
+
+    // If the snake eats food
+        if(snakeX === food.x && snakeY === food.y){
+        // Add and display new score
+            score ++;
+            scoreUpdate();
+        // Assign new food
+            let newFood = setFood();
+            food = {
+                x: newFood[0] * tile,
+                y: newFood[1] * tile
+            }
+        } else {
+        // remove tail, add to available Set
+            let poop = snake.pop();
+            emptySpaces.add(poop.x * width + poop.y)
+        }
+    // Append new head
+        snake.unshift(
+            {x: snakeX, y: snakeY}
+        )
+    }
+
+// Change direction
+document.addEventListener("keydown", direction);
+    function direction(event){
+        if(event.keyCode === 37 && d != "RIGHT"){
+            d = "LEFT";
+            draw();
+        }
+        else if(event.keyCode === 38 && d != "DOWN"){
+            d = "UP";
+            draw();
+        }
+        else if(event.keyCode === 39 && d != "LEFT"){
+            d = "RIGHT";
+            draw();
+        }
+        else if(event.keyCode === 40 && d != "UP"){
+            d = "DOWN";
+            draw();
+        }
+    }
+
+    function gameOver(){
+        const head = snake[0];
+    // check for snake head collision with wall
+        if(head.x < 1 || head.x > 11 || head.y < 1 || head.y > 11) return true;
+    // check for snake head collision with self
+        // Snake can't hit self if length < 5
+        if(snake.length < 5) return false;
+    // If head location is not in emptySpaces Set, snake has crashed into self
+        if(!emptySpaces.has(head.x * width + head.y)) return true
+
+    // Game not over. Delete new head location from Set
+        emptySpaces.delete(head.x * width + head.y)
+        return false;
+    }
+
+    function scoreUpdate() {
+        document.getElementById('score').innerText = '' + score;
+        // if(score % 5 === 0) speed *= 0.95;
+    };
+
+
+
+
+// classes board, snake, tile, food
+// Build game:
+    // Grab element from HTML
+    // Initialize variables
+        // Height and width will change to depend on user input in the future.
+        // Score, highScore keep track of scores
+        // Speed: how fast
+        // Tile: width of snake
+        // Active: Is game going on? Prevent height and width from changing mid-game/
+    // Change to be window width / (user request + 2), so long tile > minimum
+
+    // Set snake head, use emptySpaces to keep track of empty spaces
+    // Set food
+
+// Run game
+    // Turn every SPEED
+// Game over
+    // clearInterval, Active = false
+
+// Changing direction
+    // listener
+    // change direction
+    // if active, draw
+
+// FUNCTIONS:
+// Turn
+    // Draw
+        // Draw canvas
+        // Draw snake
+        // Draw food
+    // Game Over?
+        // Head hit wall?
+        // Length > 4?
+            // Head hit self?
+        // Active = false
+        // clearInterval(game);
+        //--------------------------
+        // delete head from emptySet
+    // Advance
+        // Determine new head
+            // old head + direction
+        // unshift new head
+    // Eat food?
+        // DON'T POP
+        // Reassign food
+        // update score
+        //--------------------------
+        // Pop tail
+        // Add tail to emptySet
+
+// Food assignment
+
+// Draw function
+    // Draw canvas
+    // Draw snake
+    // Draw food
+        // Snake eat? setFood
+        // Draw food
+
+    // Move snake
+        // old head position
+
+        // move
+
+    // If the snake eats food
+        // Add and display new score
+        // Assign new food
+        // remove tail, add to available Set
+    // Append new head
+
+// Change direction
+
+// Game over?
+    // check for snake head collision with wall
+    // check for snake head collision with self
+        // Snake can't hit self if length < 5
+    // If head location is not in emptySpaces Set, snake has crashed into self
+
+    // Game not over. Delete new head location from Set
+    // 
